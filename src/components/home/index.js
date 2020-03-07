f57d450b87d72950da6242df3ee19a9783c68005
Caio Augusto <caioaugustov@gmail.com>
2020-03-07 18:16:44 -0300

    Homebar

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import * as Style from './styled';
+import * as Colors from '../common/colors';
+import IconAnt from 'react-native-vector-icons/AntDesign';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HomePage = () => {
   return (
     <Style.Container>
-      
+      <Style.SafeAreaView>
+        <Style.Header>
+          <IconFeather name='menu' size={32} color={Colors.primary} />
+          <IconAnt name='twitter' size={30} color={Colors.primary} />
+          <IconMaterial name='star-four-points-outline' size={32} color={Colors.primary} />
+        </Style.Header>
+      </Style.SafeAreaView>
     </Style.Container>
   )
 }
--- src/components/home/styled/index.js
+++ src/components/home/styled/index.js
@@ -1,7 +1,23 @@
 import styled from 'styled-components'
 import * as Colors from '../../common/colors';
+import { StyleSheet } from 'react-native'
 
 export const Container = styled.View(props => ({
   flex: 1,
   backgroundColor: Colors.background
+}))
+
+export const SafeAreaView = styled.SafeAreaView(props => ({
+  alignItems: 'center'
+}))
+
+export const Header = styled.View(props => ({
+  borderBottomWidth: StyleSheet.hairlineWidth,
+  borderBottomColor: Colors.gray,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
+  paddingHorizontal: '5%',
+  paddingVertical: 10,
 }))
