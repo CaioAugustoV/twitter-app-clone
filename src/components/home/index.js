dbe9e3e447e1524f24b079a9923c27370d848e48
Caio Augusto <caioaugustov@gmail.com>
2020-03-07 17:23:54 -0300

    HomePage

---
+++ src/components/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Style from './styled';
+
+const HomePage = () => {
+  return (
+    <Style.Container>
+      
+    </Style.Container>
+  )
+}
+
+export default HomePage;
---
+++ src/components/home/styled/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import * as Colors from '../../common/colors';
+
+export const Container = styled.View(props => ({
+  flex: 1,
+  backgroundColor: Colors.background
+}))
